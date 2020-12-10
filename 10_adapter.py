9417b9e2f40cabed25cd2b47d57ec0fd4741f2ab
SnailCygnuss <sahil.a654@gmail.com>
2020-12-10 16:06:12 +0100

    Day10 code rmd  function count_joltage_combinations

--- 10_adapter.py
+++ 10_adapter.py
@@ -73,23 +73,6 @@ def count_dictionary_nesting(dictionary):
         return count_dict[keys[0]] + count_dict[keys[1]] + count_dict[keys[2]]
 
 
-def count_joltage_combinations(list_joltage):
-    complete_joltage_set = set(list_joltage)
-    first_set = {0}
-    print(find_next_joltage_set(first_set, complete_joltage_set))
-    print(find_next_joltage_set({19}, complete_joltage_set))
-    
-    present_set = first_set
-    next_set = present_set
-    list_length_set = []
-    while len(next_set) != 0:
-        next_set = find_next_joltage_set(present_set, complete_joltage_set)
-        list_length_set.append(len(next_set))
-        present_set = next_set
-    print(list_length_set)
-    print(functools.reduce(operator.mul, list_length_set))
-
-
 def find_joltage_difference(list_num):
     sorted_list = sorted(list_num)
     sorted_list.append(sorted_list[-1] + 3) # Add device voltage +3

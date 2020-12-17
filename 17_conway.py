2a02eeb8f6f2a631af16c6b219e8081dc4fcd71a
SnailCygnuss <sahil.a654@gmail.com>
2020-12-18 00:15:19 +0100

    Day17 Part two completed
    Part two takes 488s

--- 17_conway.py
+++ 17_conway.py
@@ -96,28 +96,43 @@ def main(file_name='Day_17/17_input.txt'):
     print(new_array.sum())
 
 
-def get_sorrounding_elems(input_dim):
+def get_sorrounding_elems(input_dim, d=3):
     x = (input_dim[0][0] - 1, input_dim[0][1] + 1)
     y = (input_dim[1][0] - 1, input_dim[1][1] + 1)
     z = (input_dim[2][0] - 1, input_dim[2][1] + 1)
+    if d == 4:
+        w = (input_dim[3][0] - 1, input_dim[3][1] + 1)
+        return [x, y, z, w]
     return [x, y, z]
 
 
-def get_neighbour_sum(coords, active_states):
+def get_neighbour_sum(coords, active_states, d=3):
     neighbour_sum = 0
-    for a, b, c in itertools.product([-1, 0, 1], repeat=3):
-        if a == b == c == 0:
-            continue
-        x = coords[0] - a
-        y = coords[1] - b
-        z = coords[2] - c
-        if (x, y, z) in active_states:
+    if d == 3:
+        for a, b, c in itertools.product([-1, 0, 1], repeat=3):
+            if a == b == c == 0:
+                continue
+            x = coords[0] - a
+            y = coords[1] - b
+            z = coords[2] - c
+            if (x, y, z) in active_states:
             # print(x, y, z)
-            neighbour_sum = neighbour_sum + 1
+                neighbour_sum = neighbour_sum + 1
     # print(coords)
     # print(neighbour_sum)
     # input()
-    return neighbour_sum
+        return neighbour_sum
+    elif d == 4:
+        for a, b, c, d in itertools.product([-1, 0, 1], repeat=4):
+            if a == b == c == d == 0:
+                continue
+            x = coords[0] - a
+            y = coords[1] - b
+            z = coords[2] - c
+            w = coords[3] - d
+            if (x, y, z, w) in active_states:
+                neighbour_sum = neighbour_sum + 1
+        return neighbour_sum
 
 
 def print_cubes(dims, active_states):
@@ -147,7 +162,8 @@ def main2(file_name='Day_17/17_input.txt'):
     # print(active_states)
     dims = [(0, input_dim), (0, input_dim), (0, 1)]
     
-    for _ in range(6):
+    for i in range(6):
+        print(f'Iteration {i+1}')
         new_active_states = []
         dims = get_sorrounding_elems(dims)
         # print(dims)
@@ -167,10 +183,51 @@ def main2(file_name='Day_17/17_input.txt'):
     print(len(new_active_states))
 
 
+def main3(file_name='Day_17/17_input.txt'):
+    with open(file_name, 'r') as f:
+        input_slice = list(map(str.strip, f.readlines()))
+    input_dim = len(input_slice)
+
+    active_states = []
+    for x, y, z, w in itertools.product(range(input_dim), range(input_dim), 
+                                     range(1), range(1)):
+        if input_slice[y][x] == '#':
+            active_states.append((x, y, z, w))
+
+    # print(active_states)
+    dims = [(0, input_dim), (0, input_dim), (0, 1), (0, 1)]
+    
+    for i in range(6):
+        print(f'Iteration {i+1}')
+        new_active_states = []
+        dims = get_sorrounding_elems(dims, d=4)
+        # print(dims)
+        for x, y, z, w in itertools.product(range(dims[0][0], dims[0][1]),
+                                        range(dims[1][0], dims[1][1]),
+                                        range(dims[2][0], dims[2][1]),
+                                        range(dims[3][0], dims[3][1])):
+            n_sum = get_neighbour_sum((x, y, z, w), active_states, d=4)
+            if (x, y, z, w) in active_states:
+                if (n_sum == 2) or (n_sum == 3):
+                    new_active_states.append((x, y, z, w))
+            else:
+                if n_sum == 3:
+                    new_active_states.append((x, y, z, w))
+        active_states = new_active_states.copy()
+
+    # print_cubes(dims, new_active_states)
+    print(len(new_active_states))
+
+
 if __name__ == '__main__':
     start1 = time.time()
     # main(file_name='Day_17/17_test.txt')
     # main()
     # main2(file_name='Day_17/17_test.txt')
-    main2()
+    # main2()
     print(f'Part One completed in {time.time() - start1}')
+
+    start2 = time.time()
+    # main3(file_name='Day_17/17_test.txt')
+    main3()
+    print(f'Part Two completed in {time.time() - start2}')

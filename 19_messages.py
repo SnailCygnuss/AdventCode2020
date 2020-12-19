6da9d2309649ae9bb811040ef2e0119f5144b9f0
SnailCygnuss <sahil.a654@gmail.com>
2020-12-19 22:46:37 +0100

    Day 19 Part 2 complete

--- 19_messages.py
+++ 19_messages.py
@@ -1,134 +1,126 @@
 import time
-import itertools
+import re
 
 
-PROCESSED_RULES = {}
-def process_rule(rule_no: str, rule_dict: dict) -> str:
-    
-    rule_string = rule_dict[rule_no]
+def process_rule_regex(rule_no, rule_dict, processed_recursive={}):
 
-    if rule_no in PROCESSED_RULES:
-        return PROCESSED_RULES[rule_no]
-    elif len(rule_string.strip('"')) == 1:
-        PROCESSED_RULES[rule_no] = [rule_string.strip('"')]
-        return PROCESSED_RULES[rule_no]
+    if rule_no in processed_recursive:
+        return processed_recursive[rule_no]
+    elif len(rule_dict[rule_no].strip('"')) == 1:
+        processed_recursive[rule_no] = rule_dict[rule_no].strip('"')
     else:
-        try:
-            l_rule, r_rule = list(map(str.strip, rule_string.split('|')))
-        except ValueError:
-            l_rule = rule_string.strip()
-            r_rule = ''
-
-        l_rule = l_rule.split()
-        r_rule = r_rule.split()
-
-        processed_l_rule = ['']
-        for rule in l_rule:
-            new_rules = []
-            processed_rule = process_rule(rule, rule_dict)
-            for x, y in itertools.product(range(len(processed_l_rule)), 
-                                          range(len(processed_rule))):
-                new_rules.append(processed_l_rule[x] + processed_rule[y])
-            processed_l_rule = new_rules
-
-        processed_r_rule = ['']
-        for rule in r_rule:
-            new_rules = []
-            processed_rule = process_rule(rule, rule_dict)
-            for x, y in itertools.product(range(len(processed_r_rule)), 
-                                          range(len(processed_rule))):
-                new_rules.append(processed_r_rule[x] + processed_rule[y])
-            processed_r_rule = new_rules
-
-        complete_rules = [*processed_l_rule, *processed_r_rule]
-        PROCESSED_RULES[rule_no] = complete_rules
-        return PROCESSED_RULES[rule_no]
-        # rule_list = rule_string.split()
-        # complete_rule = []
-        # processed_rule1 = []
-        # processed_rule2 = []
-        # for rule in rule_list:
-        #     if rule != '|':
-        #         # Get processed rule
-        #         rule_value = process_rule(rule, rule_dict)
-        #         if len(rule_value) == 1:
-        #             processed_rule1.append(rule_value[0])
-        #         else:
-        #             processed_rule1.append(rule_value[0])
-        #             processed_rule2.append(rule_value[1])
-        #     else:
-        #         processed_rule1 = ''.join(processed_rule1)
-        #         complete_rule.append(processed_rule1)
-        #         processed_rule1 = []
-        # # print(processed_rule1)
-        # # print(processed_rule2)
-        # processed_rule1 = ''.join(processed_rule1)
-        # processed_rule2 = ''.join(processed_rule2)
-        # complete_rule.append(processed_rule1)
-        # if processed_rule2:
-        #     complete_rule.append(processed_rule2)
-        # PROCESSED_RULES[rule_no] = complete_rule
-        # return PROCESSED_RULES[rule_no]
-    
-
-def process_rules(rule_dict):
-    processed_rule_dict = {}
-    # Find keys with single alpha char
-    for rule_num, rule in rule_dict.items():
-        if '"' in rule:
-            processed_rule = rule.strip('"')
-            processed_rule_dict[int(rule_num)] = processed_rule
-    print(processed_rule_dict)
-    # Remove rule_nums with single alpha char
+        regex_pat = '('
+        rule_string = rule_dict[rule_no].split()
+        for rule in rule_string:
+            if rule == '|':
+                regex_pat = regex_pat + ')' + '|' + '('
+                continue
+            regex_pat = regex_pat + '(' + process_rule_regex(rule, rule_dict, processed_recursive) + ')'
+        regex_pat = regex_pat + ')'
+        processed_recursive[rule_no] = regex_pat
+    return processed_recursive[rule_no]
 
 
-def main1(file_name='Day_19/19_input.txt'):
-    f_obj = open(file_name, 'r')
-    zero_matches = 0    
-
+def read_rules(f_obj):
     # Read all the rules
     rule_dict = {}
     line = next(f_obj)
     while line != '\n':
-        print(line)
+        # print(line)
         rule_num, rules = line.split(':')
         rules = rules.strip()
         rule_dict[rule_num] = rules
         line = next(f_obj)
-    print(rule_dict)
-    # Process rules
-    process_rules(rule_dict)
+    return rule_dict, f_obj
+
+
+def main1(file_name='Day_19/19_input.txt', part=1):
+    global PROCESSED_RULES
+    processed_recursive = {}
+    print(file_name)
+    f_obj = open(file_name, 'r')
+    allowed_count = 0
+
+    rule_dict, f_obj = read_rules(f_obj)    
+
+    # For part Two update the rules
+    if part == 2:
+        rule_8 = process_rule_regex('8', rule_dict)
+        rule_8 = rule_8 + '+'
+        rule_dict['11'] = ('| '.join(['42 '*i + '31 '*i for i in range(1, 10)]))
+        processed_recursive['8'] = rule_8
+        allowed_messages = process_rule_regex('0', rule_dict, processed_recursive=processed_recursive)
+    
+    # Process rules for Rule '0' as per problem
+    else:
+        allowed_messages = process_rule_regex('0', rule_dict)
+    allowed_patterns = re.compile(allowed_messages)
     # Read messages and count matching messages
+    line = next(f_obj)
+    while True:
+        if allowed_patterns.fullmatch(line.strip()):
+            allowed_count = allowed_count + 1
+        try:
+            line = next(f_obj)
+        except StopIteration:
+            break
+
     f_obj.close()
-    return zero_matches
+    return allowed_count
+
 
 
 def testcases():
-    input_text = '''0: 4 1 5
-                    1: 2 3 | 3 2
-                    2: 4 4 | 5 5
-                    3: 4 5 | 5 4
-                    4: "a"
-                    5: "b
-                    6: 5 4'''
-    input_text = input_text.split('\n')
+    PROCESSED_RULES = {}
+    ipt_text = '''0: 4 1 5
+                  1: 2 3 | 3 2
+                  2: 4 4 | 5 5
+                  3: 4 5 | 5 4
+                  4: "a"
+                  5: "b
+                  6: 5 4'''
+    opt_text = '''['aaaabb', 'aaabab', 'abbabb', 'abbbab', 'aabaab', 'aabbbb', 'abaaab', 'ababbb']
+                  ['aaab', 'aaba', 'bbab', 'bbba', 'abaa', 'abbb', 'baaa', 'babb']
+                  ['aa', 'bb']
+                  ['ab', 'ba']
+                  ['a']
+                  ['b']
+                  ['ba']'''
+    
+    input_text = ipt_text.split('\n')
+    output_text = opt_text.split('\n')
     rule_dict = {}
-    for inp in input_text:
+    rule_out = {}
+    for inp, opt in zip(input_text, output_text):
         rule_num, rule_string = inp.split(':')
         rule_dict[rule_num.strip()] = rule_string.strip()
-    # print(rule_dict)
-    print(process_rule('4', rule_dict))
-    print(process_rule('5', rule_dict))
-    print(process_rule('6', rule_dict))
-    print(process_rule('3', rule_dict))
-    print(process_rule('2', rule_dict))
-    print(process_rule('1', rule_dict))
-    print(process_rule('0', rule_dict))
+        rule_out[rule_num.strip()] = eval(opt.strip())
+            
+    rule_res = {}
+    for key in rule_dict:
+        rule_res[key] = process_rule_regex(key, rule_dict)
+    print(rule_res)
+    for key in rule_res:
+        for out in rule_out[key]:
+            pattern = re.compile(rule_res[key])
+            assert(pattern.fullmatch(out))
+    print('Test Passed')
 
 
 if __name__ == '__main__':
     start1 = time.time()
-    testcases()
-    # main1(file_name='Day_19/19_test.txt')
-    # main1()
+    # testcases()
+    # out = main1(file_name='Day_19/19_test.txt')
+    out = main1()
+    print(out)
     print(f'Part One finished in {time.time() - start1}')
+    
+
+    # Delete all values in PROCESSED_RULES beofre running each function
+    start2 = time.time()
+    # test_part2()
+    # out = main1('Day_19/19_test2.txt')
+    # out = main1('Day_19/19_test2.txt', part=2)
+    out = main1(part=2)
+    print(out)
+    print(f'Part Two finished in {time.time() - start2}')
